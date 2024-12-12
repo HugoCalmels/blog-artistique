47c65b3ec249fd237f58ed066a3885520a711baa
Hugo Calmels <calmels.hugo@yahoo.fr>
2024-12-12 18:14:13 +0100

    routes production issue

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    target: "esnext", // Utilise un format compatible avec les modules modernes
-    outDir: "dist", // Dossier de sortie
-  },
-  server: {
-    mimeTypes: {
-      "application/javascript": ["js"],
-    },
-  },
-});
+})
